3ad784680c304ddebf9d399d858ed536029f87d8
sathvik0320 <120362787+sathvik0320@users.noreply.github.com>
2023-11-15 21:20:44 +0530

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,3 @@
+import requests 
+from  telegram import  Update,InlineKeyboardMarkup ,InlineKeyboardButton ,InlineQueryResultArticle,InputTextMessageContent,InputMediaPhoto
+from telegram.ext import Updater ,CommandHandler,CallbackqueryHandler ,InlineQueryHandler ,MessageHandler ,Filteers 
